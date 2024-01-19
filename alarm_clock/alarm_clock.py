11fddc37b79b9ec9a7869b91af615439d1dafddf
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-01-19 10:06:39 +0100

    added comments and documentation to password generator

--- alarm_clock/alarm_clock.py
+++ alarm_clock/alarm_clock.py
@@ -2,8 +2,12 @@ from playsound import playsound
 import time
 
 
+# ANSI escape sequences to manipulate the terminal
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
 def alarm(seconds):
     time_elapsed = 0
+    print(CLEAR)
 
     while time_elapsed < seconds:
         time.sleep(1)
@@ -13,9 +17,12 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left}:{seconds_left}")
+        print(CLEAR_AND_RETURN)
+
+        # formating variables {:start with 0 or default value, 2 digits}
+        print(f"{minutes_left:02d}:{seconds_left:02d}")
 
 
 alarm(10)
-# playsound('hohoho.mp3')
+#playsound('hohoho.mp3')
 
--- psw_generator/psw_generator.py
+++ psw_generator/psw_generator.py
@@ -4,35 +4,60 @@ import random
 
 class PasswordGenerator:
     def __init__(self):
+        """
+        Initializes the PasswordGenerator object with default character sets for letters, numbers, and special characters.
+        """
         self.characters = string.ascii_letters
         self.nums = string.digits
         self.specials = string.punctuation
 
     def generate_password(self, min_length, digits=True, special_characters=True):
+        """
+        Generates a random password based on specified criteria.
+
+        Parameters:
+        - min_length (int): Minimum length of the generated password.
+        - digits (bool): Whether to include digits in the password (default is True).
+        - special_characters (bool): Whether to include special characters in the password (default is True).
+
+        Returns:
+        - str: The generated password.
+
+        Note: The password will include letters by default, and it may include digits and special characters based on the provided criteria.
+        """
         characters = self.characters
+
+        # Include digits in the character set if specified
         if digits:
             characters += self.nums
+
+        # Include special characters in the character set if specified
         if special_characters:
             characters += self.specials
 
         psw = ""
 
+        # Continue adding random characters to the password until the desired length is reached
         while len(psw) < min_length:
-            for char in random.choice(self.characters):
-                psw += char
+            psw += random.choice(characters)
 
         return psw
 
 
 if __name__ == '__main__':
+    # Create an instance of the PasswordGenerator class
     password_generator = PasswordGenerator()
+
     while True:
         try:
+            # Get user input for password length and criteria
             length = int(input("How long would you like your password to be: "))
             has_digits = input("Do you want to include digits (y/n)? ").lower() == "y"
             has_special_characters = input("Do you want to include special characters (y/n)? ").lower() == "y"
+
+            # Generate and print the password based on user input
             password = password_generator.generate_password(length, has_digits, has_special_characters)
-            print(password)
+            print("Generated Password:", password)
             break
         except ValueError:
             print("Incorrect password length.")
