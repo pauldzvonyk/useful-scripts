3024db84de9e5c4582c2b749b142e91fb1a94bc4
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-03-11 13:23:21 +0100

    palindrome string method
    resolved palindrom string method rebase
    conflict

--- bootstrap/bootstrap-5.3.3-dist/main.py
+++ bootstrap/bootstrap-5.3.3-dist/main.py
@@ -1,2 +1,26 @@
 print("Hello world")
 print("Hello People!")
+def isPalindrome(x):
+        original_number = x
+        reversed_number = 0
+        while x > 0:
+            digit = x % 10
+            #print(digit)
+            reversed_number = reversed_number * 10 + digit
+            x //= 10
+        if original_number == reversed_number:
+              return True
+        else:
+              return False
+        
+print(isPalindrome(121))
+
+
+# Transfotming int into string.
+def is_palindrome(n):
+    n = str(n)
+    if n == n[::-1]:
+        return True
+    else:
+        return False
+print(is_palindrome(151))
