fdfad34e2f778af56eab89672812cc1c0f83e1e0
Pavlo Dzvonyk <reptile1601@yahoo.com>
2024-03-11 11:05:40 +0100

    created new branch

--- bootstrap/bootstrap-5.3.3-dist/main.py
+++ bootstrap/bootstrap-5.3.3-dist/main.py
@@ -1,16 +1,1 @@
-def isPalindrome(x):
-        original_number = x
-        reversed_number = 0
-        while x > 0:
-            digit = x % 10
-            #print(digit)
-            reversed_number = reversed_number * 10 + digit
-            x //= 10
-            print(reversed_number)
-        print(x)
-        if original_number == reversed_number:
-              return True
-        else:
-              return False
-        
-print(isPalindrome(122))
+print("Hello world")
